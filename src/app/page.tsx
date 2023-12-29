49ff29c2c36eff404c10c3bf9b318e5a5f9ac06f
Zack Ashen <zachary.h.a@gmail.com>
2023-12-29 11:39:17 -0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Link href="/auth/signin">Sign In</Link>
+      <Link href="/auth/signup">Sign Up</Link>
+    </main>
+  );
+}
